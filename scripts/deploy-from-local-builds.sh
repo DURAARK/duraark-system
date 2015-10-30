eb5417d89ce2b7b811a491815101f30470e7b039
Martin Hecher <martin.hecher.g@gmail.com>
2015-10-30 16:42:44 +0100

    * added script that deploys from locally build images * updated submodules

---
+++ scripts/deploy-from-local-builds.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "git pull && git submodule foreach git checkout master && git submodule foreach git pull"
+git pull && git submodule foreach git checkout master && git submodule foreach git pull
+
+echo "(cd ./microservice-base && ./scripts/docker-build.sh)"
+(cd ./microservice-base && ./scripts/docker-build.sh)
+
+echo "docker-compose -f devenv-compose-build.yml build && docker-compose -f devenv-compose-build.yml up"
+docker-compose -f devenv-compose-build.yml build && docker-compose -f devenv-compose-build.yml up
