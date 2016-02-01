0c344c5e5d3c40855e68d5b4cc3991bf05c227ca
Martin Hecher <martin.hecher@fraunhofer.at>
2016-02-01 16:44:29 +0100

    updated docker dev environment

--- scripts/deploy-from-local-builds.sh
+++ scripts/deploy-from-local-builds.sh
@@ -6,5 +6,5 @@ git pull && git submodule foreach git checkout master && git submodule foreach g
 echo "(cd ./microservice-base && ./scripts/docker-build.sh)"
 (cd ./microservice-base && ./scripts/docker-build.sh)
 
-echo "docker-compose -f devenv-compose-build.yml build && docker-compose -f devenv-compose-build.yml up"
-docker-compose -f devenv-compose-build.yml build && docker-compose -f devenv-compose-build.yml up -d
+echo "docker-compose -f docker-compose-prod.yml build && docker-compose -f docker-compose-prod.yml up"
+docker-compose -f docker-compose-prod.yml build && docker-compose -f docker-compose-prod.yml up -d
